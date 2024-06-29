0934265206a84a4692d67cc091f57fbba00bb20b
zhiheng huang <zhiheng.huang@gmail.com>
2024-06-29 15:59:17 -0700

    make formatting

--- docker/milvus/standalone/list_connections.py
+++ docker/milvus/standalone/list_connections.py
@@ -3,20 +3,16 @@
 
 from pymilvus import Collection
 from pymilvus import connections, utility
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 connections.connect(
     "default",
-    host=os.getenv('MILVUS_HOST'),
+    host="localhost",
     port="19530",
     user="root",
-    password=os.getenv('MILVUS_PASSWD'),
+    password="Milvus",
 )
 print(f"All Milvus collections: {utility.list_collections()}")
 
-index = "scidocs"
+index = "nq"
 collection = Collection(index)  # Get an existing collection.
 print(f"{index} collection.num_entities: {collection.num_entities}")
