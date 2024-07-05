897f713bb8bbe8697d90b48d54a6177b574f6cc8
Ubuntu <ubuntu@ip-172-31-25-126.us-west-2.compute.internal>
2024-07-05 16:14:35 +0000

    Adapt the jinaai/jina-reranker-v2-base-multilingual model for Reranker and modify predict batch_szie  about sentence_transformers

--- denser_retriever/retriever_milvus.py
+++ denser_retriever/retriever_milvus.py
@@ -147,12 +147,12 @@ class RetrieverMilvus(Retriever):
             with open(fields_file, "r") as file:
                 self.field_cat_to_id, self.field_id_to_cat = json.load(file)
 
-    def generate_embedding(self, texts, query=False):
+    def generate_embedding(self, texts, batch_size, query=False):
         if query and not self.config.one_model:
-            embeddings = self.model.encode(texts, prompt_name="query")
+            embeddings = self.model.encode(texts, batch_size=batch_size, prompt_name="query")
             # embeddings = self.model.encode(texts, prompt="Represent this sentence for searching relevant passages:")
         else:
-            embeddings = self.model.encode(texts)
+            embeddings = self.model.encode(texts, batch_size=batch_size)
         return embeddings
 
     def ingest(self, doc_or_passage_file, batch_size):
@@ -204,7 +204,7 @@ class RetrieverMilvus(Retriever):
                         fieldss[i].append(-1)
                 record_id += 1
                 if len(batch) == batch_size:
-                    embeddings = self.generate_embedding(batch)
+                    embeddings = self.generate_embedding(batch,batch_size)
                     record = [uids, sources, titles, texts, pids, np.array(embeddings)]
                     record += fieldss
                     try:
@@ -215,7 +215,7 @@ class RetrieverMilvus(Retriever):
                         )
 
                     records_per_file.append(record)
-                    if len(records_per_file) == 1000:
+                    if len(records_per_file) == 100:
                         with open(f"{cache_file}_{record_id}.pkl", "wb") as file:
                             pickle.dump(records_per_file, file)
                         records_per_file = []
